0b05589e3bf708fe6d5e783dc640f1a65f24681b
Arturo <69714460+Jarturog@users.noreply.github.com>
2023-12-09 18:06:48 +0100

    iniciar sesión DONE 100%

--- guardar_sesion.php
+++ guardar_sesion.php
@@ -3,40 +3,46 @@
 // Obtener datos del cuerpo de la solicitud
 $data = json_decode(file_get_contents('php://input'), true);
 $correo = $data['correo'];
-$contraseña = $data['contraseña'];
+$contraseña = $data['contrasenya'];
 $tipo = $data['tipo'];
 
 if ($tipo != "vendedor" && $tipo != "comprador" && $tipo != "controlador") {
     echo json_encode(['success' => false, 'message' => 'Tipo de usuario no válido.']);
+    exit;
 }
 
 // Conexión a la base de datos
 $conn = mysqli_connect("localhost", "root", "") or die("error a conexión con el servidor");
 $db = mysqli_select_db($conn, "estimazon") or die("error a conexión con la base de datos");
+
 $consulta = "SELECT " . $tipo . ".idPersona, " . $tipo . ".contraseña FROM " . $tipo . " WHERE " . $tipo . ".correo = '$correo'";
+
 $result = mysqli_query($conn, $consulta);
-echo json_encode(['success' => false, 'message' => $consulta]);
-/*
+
+
 if (!$result) {
     echo json_encode(['success' => false, 'message' => 'Error en la consulta: ' . mysqli_error($conn)]);
+    exit;
 }
-
+mysqli_close($conn);
 $fila = mysqli_fetch_array($result);
 
 if (!$fila) {
     echo json_encode(['success' => false, 'message' => 'No se encontró ningún usuario con ese correo electrónico y tipo.']);
+    exit;
 }
 
-if ($fila[$tipo . '.contraseña'] != $contraseña) {
+if ($fila['contraseña'] != $contraseña) {
     echo json_encode(['success' => false, 'message' => 'Contraseña incorrecta.']);
+    exit;
 }
 
 // Iniciar sesión
 session_start();
-$_SESSION['idUser'] = $fila[$tipo . '.idPersona'];
+$_SESSION['idUser'] = $fila['idPersona'];
 $_SESSION['tipoUser'] = $tipo;
-session_write_close()
+// session_write_close() <----------------------- si lo descomento da error
 // Enviar respuesta al cliente
+
 echo json_encode(['success' => true]);
-mysqli_close($conn);//*/
 ?>
