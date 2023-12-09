29d569231a1aa6881afaad6bf1924cdc1b4d4e2a
Notbadtrips <martagojuan@gmail.com>
2023-12-09 22:51:05 +0100

    fuera ejemplos

--- ejemplos_iker/regimlist.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","root","HOTEL",8888) 
-    or die("Error localhost");
-$db = mysqli_select_db($con,"hotel") 
-    or die("Error database");
-echo "Funciona";
-?>
