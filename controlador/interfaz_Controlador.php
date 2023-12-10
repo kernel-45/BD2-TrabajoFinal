1fa090673a537a419589cfe23346713d0533118f
kernel-45 <ikergarciadeleon@gmail.com>
2023-12-10 17:02:19 +0100

    Update interfaz_Controlador.php

--- controlador/interfaz_Controlador.php
+++ controlador/interfaz_Controlador.php
@@ -39,71 +39,69 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="contenedor-principal">
       <div class="subtitulo">Bienvenido, controlador <?php echo $id ?></div>
       <div class="subtitulo">De qué usuario quieres consultar productos?</div>
-              <form class="miFormulario" method="POST" action="">
-            <!-- Etiqueta y campo para un input de ejemplo -->
-            <label for="ID del usuario">ID:</label>
-            <input type="number" id="idUsuarioBuscado" name="idUsuarioBuscado">
+      <form class="miFormulario" method="POST" action="">
+    <!-- Etiqueta y campo para un input de ejemplo -->
+    <label for="ID del usuario">ID:</label>
+    <input type="number" id="idUsuarioBuscado" name="idUsuarioBuscado">
 
-            <!-- Etiqueta y campo de selección -->
-            <label for="opciones">Opciones:</label>
-            <select id="opciones" name="opciones">
-                <option value="opcion1">Ver todos los pedidos</option>
-                <option value="opcion2">Ver pedidos con más de 5 días</option>
-                <!-- Más opciones según sea necesario -->
-            </select>
+    <!-- Etiqueta y campo de selección -->
+    <label for="opciones">Opciones:</label>
+    <select id="opciones" name="opciones">
+        <option value="opcion1">Ver todos los pedidos</option>
+        <option value="opcion2">Ver pedidos con más de 5 días</option>
+        <!-- Más opciones según sea necesario -->
+    </select>
 
-            <!-- Botón para enviar el formulario -->
-            <button type="submit" id="miBoton" class="miFormulario-button">Enviar</button>  
-        </form>
+    <!-- Botón para enviar el formulario -->
+    <button type="button" id="miBoton" class="miFormulario-button">Enviar</button>
+</form>
 
-    <!-- Más elementos aquí -->
-    </div>
-    <script>
+<script>
 document.addEventListener("DOMContentLoaded", function() {
     // Selecciona el botón por su ID
     var boton = document.getElementById("miBoton");
 
     // Agrega un controlador de eventos para el clic del botón
     boton.addEventListener("click", function() {
-        // Coloca aquí la funcionalidad que deseas que ocurra cuando se haga clic en el botón
-        alert("¡El botón fue clickeado!");
+        // Obtiene el valor del campo de entrada
+        var idUsuario = document.getElementById("idUsuarioBuscado").value;
+
+        // Utiliza AJAX para enviar el valor al archivo PHP
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "guardar_id.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // La respuesta del servidor puede ser manejada aquí si es necesario
+                alert("ID guardado en sesión con éxito.");
+            }
+        };
+        xhr.send("idUsuario=" + idUsuario);
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Variables de sesión de JavaScript
+    var sesionJS = {
+        opcionBusqueda: null
+    };
+
+    // Selecciona el botón por su ID
+    var boton = document.getElementById("miBoton");
+
+    // Agrega un controlador de eventos para el clic del botón
+    boton.addEventListener("click", function() {
+        // Obtiene el valor de la opción seleccionada
+        var opcionSeleccionada = document.getElementById("opciones").value;
+
+        // Guarda la opción en la variable de sesión de JavaScript
+        sesionJS.opcionBusqueda = opcionSeleccionada;
+
+        // Puedes realizar alguna acción o mostrar una alerta como confirmación
+        alert("Opción guardada en la variable de sesión de JavaScript con éxito.");
     });
 });
 </script>
 
-</body>
-<script>function enviarFormulario(formulario) {
-    // Obtener los valores del formulario
-    const formData = new FormData(formulario);
 
-    // Realizar la solicitud AJAX
-    fetch(formulario.action, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Object.fromEntries(formData)),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Manejar la respuesta del servidor aquí
-        if (data.success) {
-            // Redirigir o realizar otras acciones según sea necesario
-            alert('Operación exitosa');
-            window.location.href = 'busquedaControlador.php';
-        } else {
-            alert('Error al enviar el formulario: ' + data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Hubo un problema al procesar la solicitud. Por favor, inténtalo de nuevo más tarde.!!!!!');
-    });
-}
-</script>
+</body>
 </html>
