f8976ee7ca3c2855e1dbc7d009784ad0a0190474
kernel-45 <ikergarciadeleon@gmail.com>
2023-12-10 16:12:42 +0100

    d

--- controlador/interfaz_Controlador.php
+++ controlador/interfaz_Controlador.php
@@ -9,6 +9,17 @@ $id = $_SESSION['idUser'];
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Capturar los valores del formulario
+    session_start();
+    $idUsuarioBuscado = $_POST['idUsuarioBuscado'];
+    $opciones = $_POST['opciones'];
+
+    // Ahora puedes utilizar $idUsuarioBuscado y $opciones en tu lógica de PHP
+    // Por ejemplo, puedes procesar los datos o realizar consultas a la base de datos.
+}
 ?>
 <html>
 <head>
@@ -28,24 +39,59 @@ if ($conn->connect_error) {
     <div class="contenedor-principal">
       <div class="subtitulo">Bienvenido, controlador <?php echo $id ?></div>
       <div class="subtitulo">De qué usuario quieres consultar productos?</div>
-              <form class="miFormulario">
+              <form class="miFormulario" method="POST" action="">
             <!-- Etiqueta y campo para un input de ejemplo -->
             <label for="ID del usuario">ID:</label>
-            <input type="number" id="idUsuarioBuscado" name="isUsuarioBuscado">
+            <input type="number" id="idUsuarioBuscado" name="idUsuarioBuscado">
 
             <!-- Etiqueta y campo de selección -->
             <label for="opciones">Opciones:</label>
             <select id="opciones" name="opciones">
                 <option value="opcion1">Ver todos los pedidos</option>
-                <option value="opcion2">Ver pedidos con mas de 5 dias</option>
+                <option value="opcion2">Ver pedidos con más de 5 días</option>
                 <!-- Más opciones según sea necesario -->
             </select>
 
             <!-- Botón para enviar el formulario -->
             <button type="submit" class="miFormulario-button">Enviar</button>
+            
         </form>
 
     <!-- Más elementos aquí -->
     </div>
 </body>
+<script>function enviarFormulario(formulario) {
+    // Obtener los valores del formulario
+    const formData = new FormData(formulario);
+
+    // Realizar la solicitud AJAX
+    fetch(formulario.action, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(Object.fromEntries(formData)),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Manejar la respuesta del servidor aquí
+        if (data.success) {
+            // Redirigir o realizar otras acciones según sea necesario
+            alert('Operación exitosa');
+            window.location.href = 'busquedaControlador.php';
+        } else {
+            alert('Error al enviar el formulario: ' + data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Hubo un problema al procesar la solicitud. Por favor, inténtalo de nuevo más tarde.!!!!!');
+    });
+}
+</script>
 </html>
