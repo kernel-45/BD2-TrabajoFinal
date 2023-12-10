aae2fa98978da25ecda474fca688b35569a5f0ee
kernel-45 <ikergarciadeleon@gmail.com>
2023-12-10 16:45:51 +0100

    Update interfaz_Controlador.php

--- controlador/interfaz_Controlador.php
+++ controlador/interfaz_Controlador.php
@@ -39,59 +39,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="contenedor-principal">
       <div class="subtitulo">Bienvenido, controlador <?php echo $id ?></div>
       <div class="subtitulo">De qué usuario quieres consultar productos?</div>
-              <form class="miFormulario" method="POST" action="">
-            <!-- Etiqueta y campo para un input de ejemplo -->
-            <label for="ID del usuario">ID:</label>
-            <input type="number" id="idUsuarioBuscado" name="idUsuarioBuscado">
-
-            <!-- Etiqueta y campo de selección -->
-            <label for="opciones">Opciones:</label>
-            <select id="opciones" name="opciones">
-                <option value="opcion1">Ver todos los pedidos</option>
-                <option value="opcion2">Ver pedidos con más de 5 días</option>
-                <!-- Más opciones según sea necesario -->
-            </select>
-
-            <!-- Botón para enviar el formulario -->
-            <button type="submit" class="miFormulario-button">Enviar</button>
-            
-        </form>
+              
 
     <!-- Más elementos aquí -->
     </div>
 </body>
-<script>function enviarFormulario(formulario) {
-    // Obtener los valores del formulario
-    const formData = new FormData(formulario);
-
-    // Realizar la solicitud AJAX
-    fetch(formulario.action, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Object.fromEntries(formData)),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        // Manejar la respuesta del servidor aquí
-        if (data.success) {
-            // Redirigir o realizar otras acciones según sea necesario
-            alert('Operación exitosa');
-            window.location.href = 'busquedaControlador.php';
-        } else {
-            alert('Error al enviar el formulario: ' + data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Hubo un problema al procesar la solicitud. Por favor, inténtalo de nuevo más tarde.!!!!!');
-    });
-}
-</script>
 </html>
