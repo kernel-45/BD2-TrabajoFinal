84d38e7ead69f65fe3352b3cc80f651ca88f6a13
Notbadtrips <martagojuan@gmail.com>
2023-12-10 12:25:41 +0100

    nuevos files subir producto- perfil vendedor (no funcionan)

--- interfaz_vendedor.php
+++ interfaz_vendedor.php
@@ -24,59 +24,13 @@
       >
         Consultar productos
       </button>
-      <button class="boton">Nuevo producto</button>
-      <button class="boton">Agregar producto existente</button>
+      <button 
+        class="boton"
+        id="SubirProducto"
+        onclick="window.location.href='subirProducto.php'"
+        >
+        Nuevo producto
+      </button>
     </div>
-    <div id="listaProductos"></div>
-
-    <script>
-    const correoVendedor = '<?php echo $correoVendedor; ?>';
-      document
-        .getElementById("consultarProductos")
-        .addEventListener("click", function () {
-          fetch("gestCatalogo_vendedor.php", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              
-              correo: correoVendedor
-            }),
-          })
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Network response was not ok");
-              }
-              return response.json();
-            })
-            .then((data) => {
-              mostrarProductos(data);
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-              alert(
-                "Hubo un problema al obtener los productos: " + error.message
-              );
-            });
-        });
-
-      function mostrarProductos(productos) {
-        const contenedor = document.getElementById("listaProductos");
-        contenedor.innerHTML = "";
-
-        productos.forEach((producto) => {
-          const div = document.createElement("div");
-          div.classList.add("producto");
-
-          div.innerHTML = `<h3>${producto.nombre}</h3>
-                             <p>Precio: ${producto.precio}</p>
-                             <p>Descripción: ${producto.descripcion}</p>
-                             <p>Stock: ${producto.stock}</p>`;
-
-          contenedor.appendChild(div);
-        });
-      }
-    </script>
   </body>
 </html>
