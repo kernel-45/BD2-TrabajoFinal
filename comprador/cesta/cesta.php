7a7709ad89b64c5213ec35efc87944fb96b494d0
miguelsj <miguelsj14@gmail.com>
2023-12-12 03:06:41 +0100

    Cesta
    Añadida la primera version de la cesta

--- comprador/cesta/cesta.php
+++ comprador/cesta/cesta.php
@@ -15,6 +15,7 @@ session_start();
 $id = $_SESSION['idUser']; 
 // Obtienes los productos de la cesta
 $productosEnCesta = obtenercesta($id, $conn);
+$costoTotal = 0;
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -26,28 +27,36 @@ $productosEnCesta = obtenercesta($id, $conn);
 </head>
 <body>
 <div class="titulo">
-    <div class="botones" id="botones-comprador">
-        <button class="boton" onclick=resetAllCookies()>Cerrar sesión</button>
-        <button class="boton" onclick="location.href='../perfil.html'">Perfil</button>
-    </div>
-      CESTA
-      <div class="botones">
-        <button class="boton" onclick="location.href='../../inicio-sesion/iniciar_sesion.html'">Identifícate</button>
-        <button class="boton">
+    CESTA
+    <div class="botones">
+        <div id="botones-comprador" style="display: inline-block;">
+          <button class="boton" onclick=resetAllCookies(0)> Cerrar sesión</button>
+          <button class="boton" onclick="location.href='comprador/perfil.html'">Perfil</button>
+        </div>
+      <div class="botones" id="botones-usuario" style="margin-right: 100px;">
+          <button class="boton" onclick="location.href='inicio-sesion/iniciar_sesion.html'">Identifícate</button>
+        </div>
+        <button class="boton" onclick="location.href='cesta.php'">
           <img src="../../carrito.png" alt="Carrito" class="icono-carrito" />Cesta
         </button>
       </div>
+      </div>
     </div>
     <h1>Cesta</h1>
-    <ul class="propiedadesproducto" id="listacarrito">
+    <ul class="cesta" id="listacarrito">
+        
         <?php foreach ($productosEnCesta as $producto): ?>
-            <li>
-                <h3><?php echo $producto['idPedido']; ?></h3>
-                <p><strong>Stock:</strong> <?php echo $producto['nombre']; ?></p>
+            <li class="productoc"   >
+                <p><strong>Nombre:</strong> <?php echo $producto['nombre']; ?></p>
                 <p><strong>Descripción:</strong> <?php echo $producto['descripcion']; ?></p>
                 <p><strong>Precio:</strong> $<?php echo $producto['precio']; ?></p>
                 <p><strong>Cantidad:</strong> <?php echo $producto['qtt']; ?></p>
+                <?php
+                // Sumar el precio de cada producto al costo total
+                $costoTotal += $producto['precio'] * $producto['qtt'];
+                ?>
             </li>
+            
         <?php endforeach; ?>
         </ul>
 
