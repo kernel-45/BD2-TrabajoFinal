795e7eae9fc0ebc018121b549f99fa64fc9d1e5f
kernel-45 <ikergarciadeleon@gmail.com>
2023-12-13 23:20:58 +0100

    cambios arturo

--- zonas/domicilio_anyadido.php
+++ zonas/domicilio_anyadido.php
@@ -1,5 +1,5 @@
 <?php
-$data = json_decode(file_get_contents('php://input'), true);
+$data = json_decode(file_get_contents('php://input'), true); // capto los datos json y los decodifico
 $datos = array(
     "puerta" => $data["puerta"],
     "piso" => $data["piso"],
@@ -11,23 +11,21 @@ $datos = array(
     "localidad" => $data["localidad"],
     "cp" => $data["cp"]
 );
-$datos = array_values($datos);
+$datos = array_values($datos); // los paso a un array asociativo y me quedo con los valores
 
 // Conexión a la base de datos
 $conn = mysqli_connect("localhost","root","") or die("error a conexió amb servidor");
 $db = mysqli_select_db($conn, "estimazon") or die("error a conexió amb bd");
-
-$id_direccion = get_direccion($conn, $datos);
-session_start();
-$id_comprador = $_SESSION['idUser'];
-
-// INSERT IGNORE intenta insertar, si se encuentra claves duplicadas no salta un error, simplemente no hace nada
-$insert = "INSERT INTO r_tienedomicilioen (idComprador, idZona) VALUES (".$id_comprador.", ".$id_direccion.")";
-try {
+// la función get_direccion está abajo desarrollada
+$id_direccion = get_direccion($conn, $datos); // consigo el idZona de la dirección asociada a los datos, si no hay la creo
+session_start(); // inicio sesión
+// hago el insert que añade el domicilio al usuario con sesión iniciada
+$insert = "INSERT INTO r_tienedomicilioen (idComprador, idZona) VALUES (".$_SESSION['idUser'].", ".$id_direccion.")";
+try { // si error salta excepción
     if (!mysqli_query($conn, $insert)) {
         throw new mysqli_sql_exception(mysqli_error($conn));
     } else {
-        echo json_encode(['success' => true]);
+        echo json_encode(['success' => true]); // si todo ha ido bien se lo manda a la función AJAX (servidor -> cliente)
     }
 } catch (mysqli_sql_exception $e) {
     // Verificar si el código de error es específico de llave duplicada
@@ -40,40 +38,51 @@ try {
 // Cerrar la conexión a la base de datos
 mysqli_close($conn);
 
+/**
+ * Devuelve el idZona que apunta a la dirección de $datos
+ */
 function get_direccion($conn, $datos) {
-    $anterior = null;
-    while (!empty($datos)) {
-        $value = array_pop($datos);
-        if ($value == null) {
-            continue;
+    $anterior = null; // $anterior es el idZonaPadre del idZona, empieza sin padre porque recorre de raíz a hoja
+    while (!empty($datos)) { // mientras no lo haya procesado todo y la zona exista
+        $value = array_pop($datos); // coge el primer nombreZona (el primero de todos es el CP)
+        if ($value == null) { // si es una zona opcional (como piso o puerta)
+            continue; // pasa a procesar la siguiente zona, que será hija de la zona que iba a ser padre de esta zona saltada
         }
-        if ($anterior == null) {
-            $consulta = "SELECT zona.idZona, COUNT(*) AS total FROM zona WHERE zona.idZonaPadre IS NULL AND zona.nombreZona = '".$value."'";
-        } else {
-            $consulta = "SELECT zona.idZona, COUNT(*) AS total FROM zona WHERE zona.idZonaPadre = ".$anterior." AND zona.nombreZona = '".$value."'";
+        if ($anterior == null) { // si es una raíz encuentra el id con su nombre
+            $consulta =
+            "SELECT zona.idZona
+                FROM zona
+                    WHERE zona.idZonaPadre IS NULL
+                    AND zona.nombreZona = '".$value."'";
+        } else { // si es un nodo intermedio u hoja busca el hijo del anterior nodo zona
+            $consulta =
+            "SELECT zona.idZona
+                FROM zona
+                    WHERE zona.idZonaPadre = ".$anterior.
+                    " AND zona.nombreZona = '".$value."'";
         }
-        $result = mysqli_query($conn, $consulta);
-        if (!$result) {
+        $result = mysqli_query($conn, $consulta); // ejecuto la consulta
+        if (!$result) { // si error
             echo "Error en la consulta: ".mysqli_error($conn);
         }
-        $fila = mysqli_fetch_array($result);
-        $n_zonas = $fila['total'];
-        if ($n_zonas > 1) {
+        $n_zonas = mysqli_num_rows($result); // cuento las filas
+        if ($n_zonas > 1) { // si varias tienen el mismo nombre error
             echo "Error en la consulta: se han encontrado varias zonas idénticas";
-        } elseif ($n_zonas == 0) {
-            array_push($datos, $value);
+        } elseif ($n_zonas == 0) { // si no existe esa zona sale del bucle para añadirla
+            array_push($datos, $value); // vuelve a meterla en el array para poder procesarla
             break;
         }
-        $anterior = $fila['idZona'];
+        $fila = mysqli_fetch_array($result); // capto la fila
+        $anterior = $fila['idZona']; // guardo el valor del id para poder acceder a su hijo en la siguiente iteración
     }
-    while (!empty($datos)) {
-        $value = array_pop($datos);
-        if ($value == null) {
+    while (!empty($datos)) { // mientras no haya terminado de procesar la dirección va insertando zonas
+        $value = array_pop($datos); // proceso la siguiente zona
+        if ($value == null) { // si no ha sido indicada paso a la siguiente
             continue;
         }
-        if ($anterior == null) {
+        if ($anterior == null) { // si la zona no tiene padre no lo añade porque el valor predeterminado es null
             $insert = "INSERT INTO zona (nombreZona) VALUES ('".$value."')";
-        } else {
+        } else { // si tiene padre lo añade junto al nombre de la zona, en ambos casos el idZona se genera de forma autoincremental
             $insert = "INSERT INTO zona (nombreZona, idZonaPadre) VALUES ('".$value."', ".$anterior.")";
         }
         if (!mysqli_query($conn, $insert)) {
